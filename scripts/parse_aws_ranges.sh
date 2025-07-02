11f385051a3fda6c68ac02ad4fecdaacccc9f81e
Zdravko Petričušić <zpetricusic@span.eu>
2025-07-02 10:41:07 +0200

    Added AWS all ranges CSV file

--- scripts/parse_aws_ranges.sh
+++ scripts/parse_aws_ranges.sh
@@ -6,6 +6,7 @@ AWS_DIRECTORY="$DATA_DIRECTORY/aws"
 AWS_RANGES_URL="https://ip-ranges.amazonaws.com/ip-ranges.json"
 AWS_RANGES_FILE=$(echo -n "$AWS_RANGES_URL" | cut -d "/" -f 4)
 AWS_API_GATEWAY_FILE="api_gateway.json"
+AWS_RANGES_CSV_FILE="ip-ranges.csv"
 
 # Amazon Web Services
 if [[ ! -d AWS_DIRECTORY ]]; then
@@ -22,3 +23,6 @@ echo "[+] AWS ranges fetched"
 
 ## AWS gateway ranges
 jq '.prefixes[] | select(.service == "API_GATEWAY")' "$AWS_DIRECTORY/$AWS_RANGES_FILE" > "$AWS_DIRECTORY/$AWS_API_GATEWAY_FILE"
+
+## All AWS ranges as a csv
+jq -r '.prefixes[].ip_prefix' "$AWS_DIRECTORY/$AWS_RANGES_FILE" | sort -V > "$AWS_DIRECTORY/$AWS_RANGES_CSV_FILE"
